4bb5060bd9a92fe7b0a187754d3b4a57d8cfdbda
zhangtaojun <zhangtaojun@leetcode.cn>
2024-10-16 18:41:23 +0800

    feat: update code

--- Core/Screens/RoomScreen.cs
+++ Core/Screens/RoomScreen.cs
@@ -1,5 +1,4 @@
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Input;
 using MonoGame.Extended.Collections;
 using MonoGame.Extended.Screens;
 
@@ -8,7 +7,7 @@ class RoomScreen(MyGame game) : GameScreen(game)
     private readonly Node world = new() { localPosition = MyGame.ScreenCenter };
     private readonly Node myCardsNode = new() { localPosition = new(0, MyGame.ScreenHeight / 2 - 100) };
     private readonly List<Card> allCards = new Cards().cards;
-
+    private readonly List<Card> myCards = [];
 
     public override void Initialize()
     {
@@ -16,36 +15,23 @@ class RoomScreen(MyGame game) : GameScreen(game)
         world.Children.Insert(0, bg);
 
         var cardBackAtlas = new SpriteAtlas(Assets.TextureCardsBack) { regionWidth = 88, regionHeight = 124, maxRegionCount = 2 }.GetAtlas();
-        var cardBackNode = new SpriteRegionNode() { texture2DRegion = cardBackAtlas[0], scale = new(0.8f) };
+        var cardBackNode = new SpriteRegionNode() { tag = "CardBackNode", texture2DRegion = cardBackAtlas[0], scale = new(0.8f) };
         world.NodeManager.AddChild(cardBackNode);
 
-        world.NodeManager.AddChild(new LabelNode() { tag = "CardsCount", text = $"剩余{allCards.Count}张牌", localPosition = new(0, 80) });
+        world.NodeManager.AddChild(new LabelNode() { tag = "CardsCountLabel", text = $"剩余{allCards.Count}张牌", localPosition = new(0, 80) });
 
-        // PickCard();
-        var emptyNode = new Node();
-        world.NodeManager.AddChild(emptyNode);
-        emptyNode.ComponentManager.AddComponent(new ContinuousClockComponent()
+        world.ComponentManager.AddComponent(new ContinuousClockComponent()
         {
-            intervalSeconds = 1,
+            intervalSeconds = 0.2f,
             Tick = (object sender, EventArgs e) =>
             {
                 PickCard();
             }
         });
 
-        // world.NodeManager.AddChild(myCardsNode);
-
-        // var singleCardNode = new SpriteRegionNode() { texture2DRegion = allCards[0].Texture2DRegion, scale = new(0.8f), localPosition = new(0, -(MyGame.ScreenHeight / 2 - 100)) };
-        // singleCardNode.ComponentManager.AddComponent(new TweenerComponent()
-        // {
-        //     tweenerAction = (t, n) =>
-        //     {
-        //         t.TweenTo(n, e => e.localPosition, new(0, 0), 0.2f);
-        //     }
-        // });
+        world.NodeManager.AddChild(myCardsNode);
 
         // myCardsNode.NodeManager.AddChild(singleCardNode);
-
         // var cardBlackClubAtlas = new SpriteAtlas(Assets.TextureCardsBlackClubs) { regionWidth = 88, regionHeight = 124, maxRegionCount = 13 }.GetAtlas();
         // var cardNode = new SpriteRegionNode() { texture2DRegion = cardBlackClubAtlas[cardIndex] };
         // cardNode.ComponentManager.AddComponent(new ContinuousClockComponent()
@@ -74,12 +60,6 @@ class RoomScreen(MyGame game) : GameScreen(game)
     public override void Update(GameTime gameTime)
     {
         world.Update(gameTime);
-
-        if (MyGame.KeyboardState.WasKeyPressed(Keys.Space))
-        {
-            var a = myCardsNode.Children[0] as SpriteRegionNode;
-            a.texture2DRegion = allCards[Random.Shared.Next(13 * 4)].Texture2DRegion;
-        }
     }
 
     public override void Draw(GameTime gameTime)
@@ -92,10 +72,40 @@ class RoomScreen(MyGame game) : GameScreen(game)
 
     private void PickCard()
     {
+        if (allCards.Count <= 0)
+        {
+            var cardBackNode = world.NodeManager.GetChildByTag<SpriteRegionNode>("CardBackNode");
+            cardBackNode?.RemoveFromParent();
+            return;
+        }
+
         allCards.Shuffle(Random.Shared);
         var card = allCards[0];
         allCards.Remove(card);
-        var cardsCountLabelNode = world.NodeManager.GetChildByTag<LabelNode>("CardsCount");
+
+
+        // singleCardNode.ComponentManager.AddComponent(new TweenerComponent()
+        // {
+        //     tweenerAction = (t, n) =>
+        //     {
+        //         t.TweenTo(n, e => e.localPosition, new(0, 0), 0.2f);
+        //     }
+        // });
+
+        myCards.Add(card);
+
+        var cardsCountLabelNode = world.NodeManager.GetChildByTag<LabelNode>("CardsCountLabel");
         cardsCountLabelNode.text = $"剩余{allCards.Count}张牌";
+
+        myCardsNode.NodeManager.RemoveAllChildren();
+
+        var len = myCards.Count;
+        for (int i = 0; i < len; i++)
+        {
+            var myCard = myCards[i];
+            var x = i - (len - 1) / 2;
+            var singleCardNode = new SpriteRegionNode() { texture2DRegion = myCard.Texture2DRegion, scale = new(0.8f), localPosition = new(x * 40, 0) };
+            myCardsNode.NodeManager.AddChild(singleCardNode);
+        }
     }
 }
